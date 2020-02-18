0ecd0e5dbb28a1d32c70142060c31444fbcadc04
Niccolum <nikolai_vidov@epam.com>
2020-02-18 16:18:54 +0300

    add multiprocessing

--- socket_http_server/core.py
+++ socket_http_server/core.py
@@ -3,6 +3,17 @@ import select
 from typing import Tuple
 from pathlib import Path
 import multiprocessing as mp
+from contextlib import contextmanager
+
+
+@contextmanager
+def lockwrapper(lock):
+    try:
+        lock.acquire()
+        yield
+    finally:
+        lock.release()
+
 
 from socket_http_server.structures import Connection
 
@@ -11,8 +22,6 @@ class HTTPServer:
         self.port = port
         self.base_directory = Path(__file__).resolve() / base_directory
         self.processes = processes
-        self. manager = mp.Manager()
-        self._connections = self.manager.dict()
         self.server_socket = None
         self.epoll = None
 
@@ -26,82 +35,86 @@ class HTTPServer:
         # Указываем серверному сокету начать прием входящих соединений от клиентов.
         _clients_queue = 50
         self.server_socket.listen(_clients_queue)
-        # Блокирующие по умолчанию сокеты нужно использовать в неблокирующем (асинхронном) режиме.
-        self.server_socket.setblocking(False)
-        self.epoll = select.epoll()
-        # Подписываемся на события чтения на серверном сокете.
-        # Событие чтения происходит в тот момент, когда серверный сокет принимает подключение.
-        self.epoll.register(self.server_socket.fileno(), select.EPOLLIN)
 
     def __enter__(self) -> Tuple[socket.socket, select.epoll]:
         self._create_server()
-        return self.server_socket, self.epoll
+        return self.server_socket
 
     def __exit__(self, type, val, traceback) -> None:
-        self.epoll.unregister(self.server_socket.fileno())
-        self.epoll.close()
         self.server_socket.close()
 
 
-    def _register_new_connection(self) -> None:
-        with mp.Lock():
+    def _register_new_connection(self, epoll: select.epoll, _connections: dict, lock: mp.Lock) -> None:
+        with lockwrapper(lock):
             curr_conn, _ = self.server_socket.accept()
             curr_conn.setblocking(False)
-            # Подписываемся на события чтения (EPOLLIN) на новом сокете.
-            self.epoll.register(curr_conn.fileno(), select.EPOLLIN)
-            self._connections[curr_conn.fileno()] = Connection(client=curr_conn)
+        # Подписываемся на события чтения (EPOLLIN) на новом сокете.
+        epoll.register(curr_conn.fileno(), select.EPOLLIN)
+        _connections[curr_conn.fileno()] = Connection(client=curr_conn)
 
 
-    def _read_new_data_for_connection(self, fileno: int):
+    def _read_new_data_for_connection(self, fileno: int, epoll: select.epoll, _connections: dict):
         eols = (b'\n\n', b'\n\r\n')
-        curr_conn = self._connections[fileno]
-        with mp.Lock():
-            curr_conn.raw_request += curr_conn.client.recv(1024)
+        curr_conn = _connections[fileno]
+        curr_conn.raw_request += curr_conn.client.recv(1024)
         if any(eol in curr_conn.raw_request for eol in eols):
             curr_conn.parse_request(self.base_directory)
-            self.epoll.modify(fileno, select.EPOLLOUT)
+            epoll.modify(fileno, select.EPOLLOUT)
 
 
-    def _write_data_for_request(self, fileno: int):
-        curr_conn = self._connections[fileno]
+    def _write_data_for_request(self, fileno: int, epoll: select.epoll, _connections: dict):
+        curr_conn = _connections[fileno]
         bytes_written = curr_conn.client.send(curr_conn.response.raw)
         curr_conn.response.raw = curr_conn.response.raw[bytes_written:]
         if len(curr_conn.response.raw) == 0:
-            self.epoll.modify(fileno, select.EPOLLHUP)
+            epoll.modify(fileno, select.EPOLLHUP)
             curr_conn.client.shutdown(socket.SHUT_RDWR)
 
+    def _close_request(self, fileno: int, epoll: select.epoll, _connections: dict):
+        epoll.unregister(fileno)
+        _connections[fileno].client.close()
+        del _connections[fileno]
 
-    def _close_request(self, fileno: int):
-        self.epoll.unregister(fileno)
-        self._connections[fileno].client.close()
-        del self._connections[fileno]
-
-
-    def _loop(self) -> None:
-        while True:
-            events = self.epoll.poll(1)
-            for fileno, event in events:
-                # if new socket session
-                if fileno == self.server_socket.fileno():
-                    self._register_new_connection()
-                # Available for read
-                elif event & select.EPOLLIN:
-                    self._read_new_data_for_connection(fileno)
-                # Available for write
-                elif event & select.EPOLLOUT:
-                    self._write_data_for_request(fileno)
-                # Available for close
-                elif event & select.EPOLLHUP:
-                    self._close_request(fileno)
+    def _loop(self, lock) -> None:
+        epoll = select.epoll()
+        # Подписываемся на события чтения на серверном сокете.
+        # Событие чтения происходит в тот момент, когда серверный сокет принимает подключение.
+        epoll.register(self.server_socket.fileno(), select.EPOLLIN)
+        _connections = {}
+        try:
+            while True:
+                events = epoll.poll(1)
+                for fileno, event in events:
+                    # if new socket session
+                    if fileno == self.server_socket.fileno():
+                        self._register_new_connection(epoll, _connections, lock)
+                    # Available for read
+                    elif event & select.EPOLLIN:
+                        self._read_new_data_for_connection(fileno, epoll, _connections)
+                    # Available for write
+                    elif event & select.EPOLLOUT:
+                        self._write_data_for_request(fileno, epoll, _connections)
+                    # Available for close
+                    elif event & select.EPOLLHUP:
+                        self._close_request(fileno, epoll, _connections)
+        finally:
+            epoll.unregister(self.server_socket.fileno())
+            epoll.close()
 
 
     def run(self) -> None:
         with self:
-            workers = [mp.Process(target=self._loop) for _ in range(self.processes)]
-            for p in workers:
-                p.daemon = True
-                p.start()
-                print(f'Worker {p.name} are ready...')
+            try:
+                lock = mp.Lock()
+                workers = [mp.Process(target=self._loop, args=(lock,)) for _ in range(self.processes)]
+                for p in workers:
+                    p.daemon = True
+                    p.start()
+                    print(f'Worker {p.name} are ready...')
 
-            for p in workers:
-                p.join()
+                for p in workers:
+                    p.join()
+
+            finally:
+                for p in workers:
+                    p.terminate()
